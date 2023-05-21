490dbd4f2a38d4b55736d4b02186b1399daacb14
Ron <gilad929@gmail.com>
2023-05-21 22:38:04 +0300

    Fix OS specific path when creating model

--- boilercam/create-model-fastai.py
+++ boilercam/create-model-fastai.py
@@ -4,9 +4,9 @@ from utils import label_func
 
 path = Path(__file__).parent
 
-path = str(path.absolute())  # Needed in order to avoid serializing OS specific Path properties, so that this is cross platform
+path = '.'  # Needed in order to avoid serializing OS specific Path properties, so that this is cross platform
 
-file_names = get_image_files(path)
+file_names = get_image_files(path, folders=["create_model"])
 
 dls = ImageDataLoaders.from_name_func(
     path, file_names, valid_pct=0.2, seed=42,
